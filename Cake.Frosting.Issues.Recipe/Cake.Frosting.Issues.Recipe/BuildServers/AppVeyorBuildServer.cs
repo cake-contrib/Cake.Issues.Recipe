ed1139837028db293b1abdeef1877bffc17542d9
Pascal Berger <pascal.berger@swissgrc.com>
2023-12-17 12:11:28 +0100

    Use switch expressions

--- Cake.Frosting.Issues.Recipe/Cake.Frosting.Issues.Recipe/BuildServers/AppVeyorBuildServer.cs
+++ Cake.Frosting.Issues.Recipe/Cake.Frosting.Issues.Recipe/BuildServers/AppVeyorBuildServer.cs
@@ -20,14 +20,14 @@ namespace Cake.Frosting.Issues.Recipe
         {
             context.NotNull(nameof(context));
 
-            switch (context.AppVeyor().Environment.Repository.Provider)
+            return context.AppVeyor().Environment.Repository.Provider switch
             {
-                case "bitBucket": return new Uri($"https://bitbucket.org/{context.AppVeyor().Environment.Repository.Name}/src");
-                case "gitHub": return new Uri($"https://github.com/{context.AppVeyor().Environment.Repository.Name}.git");
-                case "gitLab": return new Uri($"https://gitlab.com/{context.AppVeyor().Environment.Repository.Name}.git");
-                case "vso": return new Uri($"https://dev.azure.com/{context.AppVeyor().Environment.Repository.Name}");
-                default: return new Uri(context.AppVeyor().Environment.Repository.Name);
-            }
+                "bitBucket" => new Uri($"https://bitbucket.org/{context.AppVeyor().Environment.Repository.Name}/src"),
+                "gitHub" => new Uri($"https://github.com/{context.AppVeyor().Environment.Repository.Name}.git"),
+                "gitLab" => new Uri($"https://gitlab.com/{context.AppVeyor().Environment.Repository.Name}.git"),
+                "vso" => new Uri($"https://dev.azure.com/{context.AppVeyor().Environment.Repository.Name}"),
+                _ => new Uri(context.AppVeyor().Environment.Repository.Name),
+            };
         }
 
         /// <inheritdoc />
